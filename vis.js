bea53ae5da2bc1321632f1b8be79410847060ef3
AndreasAalbers <aaal0002@student.monash.edu>
2023-10-19 02:04:09 +1100

    Json Files
    I have created all the required json files for each idiom. Also updated index.html and vis.js as I have been testing out the Bubble chart.

--- vis.js
+++ vis.js
@@ -1,10 +1,10 @@
-var vg_1 = "json/map.json";
+var vg_1 = "json/Densitymap.json";
 vegaEmbed('#map', vg_1, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
-var vg_2 = "json/unemployment.json";
+var vg_2 = "json/Barchart.json";
 vegaEmbed('#unemployment', vg_2, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -14,12 +14,12 @@ vegaEmbed('#Bubblechart', vg_3, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "json/emissions.json";
+var vg_3 = "json/Piechart.json";
 vegaEmbed('#emissions', vg_3, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_4 = "json/internet.json";
+var vg_4 = "json/Wagemap.json";
 vegaEmbed('#internet', vg_4, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
